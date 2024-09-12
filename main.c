724753734301f5b641ef395cfb90f8e6e6308acb
Daybel-r <daybelperez617@gmail.com>
2024-09-12 15:05:42 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+/* Aplicacion de operadores.*/
+
+void main(void)
 {
-    printf("Hello Profesor!\n");
-    return 0;
+ int i = 5, j = 4, k, l, m;
+
+ k = !i * 3 + --j * 2 - 3;
+ printf("\nEl valor de k es: %d", k);
+
+ l = ! (!i|| (1 && 0)) && 1;
+ printf("\nEl valor de l es: %d", l);
+
+k = !(12 > 10);
+j = (10 || 0)&& k;
+m = (!(k|| j));
+printf("\nEl valor de m es: %d", m);
+return 0;
 }
