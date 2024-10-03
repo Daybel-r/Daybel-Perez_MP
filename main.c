5357b22dbb8150c115666654255171b7b1d0a64b
Daybel-r <daybelperez617@gmail.com>
2024-10-03 10:22:08 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,58 @@
 #include <stdio.h>
 
-/* Numeros perfectos.
-El programa, al recibir como dato un numero entero positivo como limite, obtiene
-los numeros perfectos que hay entre 1 y ese numero, y ademas imprime cuantos
-numeros perfectos hay en el intervalo.
+/* Funciones y parametros. */
 
-I, J, NUM, C: variables de tipo entero. */
+int z, y;                           /* Variables globales. */
 
-voidmain(void)
+int F1 (float);
+void F2 (float, int *);             /* Prototipos de funciones. */
+
+void main (void)
 {
-int I, J, NUM, SUM, C =0;//I Es el numero a verificar, J es el divisor, NUM es el limite, SUM almacena la suma de divisores, C cuenta los numeros perfectos.
-printf("\nIngrese el numero limite: ");
-scanf("%d", &NUM);// Solicita y lee el numero limite ingresado por el usuario.
-for(I = 1; I <= NUM; I++)// Ciclo para encontrar los divisores de I
+int w;
+float x;
+z = 5;
+y = 7;
+w = 2;
+x = (float)y /z;
+printf("\nPrograma Principal: %d %d %.2f %d", z, y, x, w);
+F2 (x, &w);
+printf("\nPrograma Principal: %d %d %.2f %d", z, y, x, w);
+}
+
+int F1(float x)
 {
-    SUM = 0;// Inicializa la suma de divisores en 0 para cada nuevo numero I.
-    for (J = 1; J <= (I /2); J++) // Ciclo para encontrar los divisores de I
-    if ((I % J)== 0) //Si J es divisor del I (el residuo del I divido por J es 0
-            SUM += J; // Suma el divisor J a la variable SUM.
-    if (SUM == I)// Si la suma de los divisores es igual I, entonces es un numero perfecto
-    {
-        printf("\n%d es un numero perfecto", I);// imprime que I es un numero perfecto.
-        C++;// Incrementa el contador de numeros perfectos.
-        }
-    }
-    printf("\nEntres 1 y %d hay %d numeros perfectos", NUM, C);// Imprime el numero total  de numeros perfectos encontrados en el intervalo.
-    }
+int k;
+if (x!= 0)
+{
+    k = z - y;
+    x++;
+}
+else
+   k = z + y;
+printf("\nF1: %d %d %.2f %d", z, y, x, k);
+return (k);
+}
+void F2(float T, int *r)
+{
+int y;
+y = 5;
+z = 0;
+
+printf("\nF2: %d %d %.2f %d", z, y, t, *r);
+if (z == 0)
+{
+    z = (*r) * 2;
+    t = (float) z / 3;
+    printf("\nIngresa el valor: ");
+    scanf("%d", r);             /* El usuario debe ingresar el valor 6 */
+    printf("\nF2; %d %d %.2f %d", z, y, t, *r);
+}
+else
+{
+
+    z = (*r) * 2;
+    printf("\nF2: %d %d %.2f %d", z, y, t, *r);
+}
+*r = F1(t);
+}
